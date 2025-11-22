dc14ac856c497d700236ae727fb0f831c850df75
Helen-42 <hahonec1@boe.mono.k12.wv.us>
2025-11-22 17:05:39 -0500

    maps fix
    fixed a small error- map should be working fine now

--- mapview/urls.py
+++ mapview/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.gt_map, name="gt_map"),
+    path('jobs.json', views.jobs_json, name='jobs_json'),
 ]
--- mapview/views.py
+++ mapview/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from jobs.models import Job  # OR whatever your model is
+
+def jobs_json(request):
+    jobs = Job.objects.all()
+
+    data = []
+    for job in jobs:
+        if job.latitude is not None and job.longitude is not None:
+            data.append({
+                "id": job.id,
+                "title": job.title,
+                "description": job.description,
+                "latitude": job.latitude,
+                "longitude": job.longitude,
+            })
+
+    return JsonResponse(data, safe=False)
+
 
 def gt_map(request):
     return render(request, 'mapview/map.html')
