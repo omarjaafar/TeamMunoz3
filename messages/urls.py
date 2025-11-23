9ca0661e075e3342263d231b6163ceba1878f13c
Mariann Glasier <mariannyg23@gmail.com>
2025-11-23 18:28:41 -0500

    last min fix for messages

--- messages/urls.py
+++ messages/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('send_email/', views.send_email, name='messages.send_email'),
     path('email_inbox/', views.email_inbox, name='messages.email_inbox'),
     path('email/<int:email_id>/', views.email_thread, name='messages.email_thread'),
+    path('email/inbound/', views.receive_inbound_email, name='messages.email_inbound'),
     path('thread/<int:user_id>/', views.thread_view, name='messages.thread'),
     path('send/', views.send_message, name='messages.send_message'),
 ]
